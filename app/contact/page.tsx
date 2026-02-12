c96b94a30f81405953f3afc45de38bd3de85e039
Rayan Sheikh <w2094027@westminster.ac.uk>
2026-02-12 17:46:48 +0000

    Update email contact from rayansheikh121@email.com to rayansheikh121@gmail.com

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,7 +7,7 @@ export default function ContactPage() {
                 <p className="text-gray-600 mt-2">Feel free to reach out to me through any of the platforms below:</p>
             </header>
             <ul className="space-y-2 text-black">
-                <li>Email: <a href="mailto:rayansheikh121@email.com" className="text-blue-600">rayansheikh121@email.com</a></li>
+                <li>Email: <a href="mailto:rayansheikh121@gmail.com" className="text-blue-600">rayansheikh121@gmail.com</a></li>
                 <li>LinkedIn: <a href="https://linkedin.com/in/rayan-sheikh" target="_blank" className="text-blue-600">linkedin.com/in/rayan-sheikh</a></li>
                 <li>GitHub: <a href="https://github.com/RayanSheikh01" target="_blank" className="text-blue-600">github.com/RayanSheikh01</a></li>
             </ul>
